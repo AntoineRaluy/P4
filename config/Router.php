de2ece980dd868bc5e5e8b6cefcdc60bd6ab882f
AntoineRaluy <57936050+AntoineRaluy@users.noreply.github.com>
2020-06-30 11:33:24 +0200

    add switch condition

--- config/Router.php
+++ config/Router.php
@@ -22,69 +22,102 @@ class Router
     public function run()
     {
         try {
-            if (isset($_GET['action'])) {
-                if ($_GET['action'] == 'listPosts') {
+            switch($_GET['action']) {
+                case 'listPosts':
                     $this->frontController->listChapters();
-                }
-                elseif ($_GET['action'] == 'post') {
-                    if (isset($_GET['chapterId']) && $_GET['chapterId'] > 0) {
-                        $this->frontController->chapter($_GET['chapterId']);
+                    break;
+                case 'post':
+                    $this->frontController->chapter($_GET['chapterId']);
+                    break;
+                case 'addComment':
+                    if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+                        $this->frontController->addComment($_GET['chapterId'], $_POST['author'], $_POST['comment']);
                     }
-                    else {
-                        $this->errorController->errorNotFound();
-                        // throw new Exception('Page inconnue');
-                    }
-                }
-                elseif ($_GET['action'] == 'addComment') {
-                    if (isset($_GET['chapterId']) && $_GET['chapterId'] > 0) {
-                        if (!empty($_POST['author']) && !empty($_POST['comment'])) {
-                            $this->frontController->addComment($_GET['chapterId'], $_POST['author'], $_POST['comment']);
-                        }
-                        else {
-                            $this->errorController->errorEmpty();
-                            // throw new Exception('Tous les champs ne sont pas remplis !');
-                        }
-                    }
-                    else {
-                        $this->errorController->errorNotFound();
-                        // throw new Exception('Page inconnue');
-                    }
-                }
-                elseif ($_GET['action'] === 'reportComment') {
-                    echo 'test';
+                    break;
+                case 'reportComment':
                     $this->frontController->flagComment($_GET['commentId'], $_GET['chapterId']);
-                    // if (isset($_GET['commentId']) && $_GET['commentId'] > 0) {
-                    //     $this->frontController->flagComment($_GET['commentId'], $_GET['chapterId']);
-                    // }
-                    // else {
-                    //     $this->errorController->errorNotFound();
-                    //     // throw new Exception('Aucun identifiant de commentaire envoyé');
-                    // }
-                }
-                elseif ($_GET['action'] === 'deleteComment') {
-                    if (isset($_GET['commentId']) && $_GET['commentId'] > 0) {
-                        $this->backController->deleteComment($_GET['commentId'], $_GET['chapterId']);
-                    }
-                    else {
-                        $this->errorController->errorNotFound();
-                        // throw new Exception('Aucun identifiant de commentaire envoyé');
-                    }
-                }
-                elseif ($_GET['action'] == 'addPost') {
+                    break;
+                case 'deleteComment':
+                    $this->backController->deleteComment($_GET['commentId'], $_GET['chapterId']);
+                    break;
+                case 'addPost':
                     $this->backController->addChapter($_POST);
-                }
-                elseif ($_GET['action'] == 'editPost') {
+                    break;
+                case 'editPost':
                     $this->backController->editChapter($_GET['chapterId']);
-                }
-            }
-            else {
-                $this->frontController->listChapters();
+                    break;
+                default:
+                    $this->frontController->listChapters();
             }
         }
         
         catch(Exception $e) { 
             $this->errorController->errorServer();
-            // echo 'Erreur : ' . $e->getMessage();
         }
+
+        //     if (isset($_GET['action'])) {
+        //         if ($_GET['action'] == 'listPosts') {
+        //             $this->frontController->listChapters();
+        //         }
+        //         elseif ($_GET['action'] == 'post') {
+        //             if (isset($_GET['chapterId']) && $_GET['chapterId'] > 0) {
+        //                 $this->frontController->chapter($_GET['chapterId']);
+        //             }
+        //             else {
+        //                 $this->errorController->errorNotFound();
+        //                 // throw new Exception('Page inconnue');
+        //             }
+        //         }
+        //         elseif ($_GET['action'] == 'addComment') {
+        //             if (isset($_GET['chapterId']) && $_GET['chapterId'] > 0) {
+        //                 if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+        //                     $this->frontController->addComment($_GET['chapterId'], $_POST['author'], $_POST['comment']);
+        //                 }
+        //                 else {
+        //                     $this->errorController->errorEmpty();
+        //                     // throw new Exception('Tous les champs ne sont pas remplis !');
+        //                 }
+        //             }
+        //             else {
+        //                 $this->errorController->errorNotFound();
+        //                 // throw new Exception('Page inconnue');
+        //             }
+        //         }
+        //         elseif ($_GET['action'] === 'reportComment') {
+        //             echo 'test';
+        //             $this->frontController->flagComment($_GET['commentId'], $_GET['chapterId']);
+        //             // if (isset($_GET['commentId']) && $_GET['commentId'] > 0) {
+        //             //     $this->frontController->flagComment($_GET['commentId'], $_GET['chapterId']);
+        //             // }
+        //             // else {
+        //             //     $this->errorController->errorNotFound();
+        //             //     // throw new Exception('Aucun identifiant de commentaire envoyé');
+        //             // }
+        //         }
+        //         elseif ($_GET['action'] === 'deleteComment') {
+        //             if (isset($_GET['commentId']) && $_GET['commentId'] > 0) {
+        //                 $this->backController->deleteComment($_GET['commentId'], $_GET['chapterId']);
+        //             }
+        //             else {
+        //                 $this->errorController->errorNotFound();
+        //                 // throw new Exception('Aucun identifiant de commentaire envoyé');
+        //             }
+        //         }
+        //         elseif ($_GET['action'] == 'addPost') {
+        //             $this->backController->addChapter($_POST);
+        //         }
+        //         elseif ($_GET['action'] == 'editPost') {
+        //             $this->backController->editChapter($_GET['chapterId']);
+        //         }
+        //     }
+        //     else {
+        //         $this->frontController->listChapters();
+        //     }
+        // }
+        
+        // catch(Exception $e) { 
+        //     $this->errorController->errorServer();
+        //     // echo 'Erreur : ' . $e->getMessage();
+        // }
     }
 }
